0b3c04f0e47ec38537855090e99ab79f5bfd8690
Q9Kawaii <yashdingar17@gmail.com>
2025-06-17 14:08:00 +0530

    fixed the background blob color

--- app/components/DashboardComponents/AnimatedBlob.js
+++ app/components/DashboardComponents/AnimatedBlob.js
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function AnimatedBlob() {
   return (
     <motion.div
-      className="absolute w-72 lg:w-92 h-72 lg:h-92 bg-blue-300  rounded-full mix-blend-multiply filter opacity-40 animate-blob lg:mr-10"
+      className="absolute w-72 lg:w-92 h-72 lg:h-92 bg-[$6bc5e0]  rounded-full mix-blend-multiply filter opacity-40 animate-blob lg:mr-10"
       initial={{ borderRadius: "42% 58% 70% 30% / 30% 30% 70% 70%" }}
       animate={{
         borderRadius: [
