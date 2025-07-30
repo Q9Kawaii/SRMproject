8257fb65a604e478bd43b828067fe37cfac44a61
Q9Kawaii <yashdingar17@gmail.com>
2025-07-30 08:37:30 +0530

    pending achievements done #2

--- app/achievementsPage/adminachievementpage/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import AdminAchievementDashboard from "./AdminAchievementDashboard";
-
-export default function AchievementsPage() {
-  return (
-    <AdminAchievementDashboard/>
-  );
-}
